4dba42778000c49e19d87a2beaa5e59ffa66e0f2
FEUE256 <vargard36@gmail.com>
2024-06-18 16:31:36 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,47 @@
 print("Hello World")
+
+a = Animation()
+background(Y)
+
+def set_pixel():
+  for set in range(8):
+     m[set][set] = R
+    
+
+set_pixel()
+
+def set_pixel_2():
+  for set_2 in range(8):
+    set = 7- set_2
+    m[set][set_2] = R
+    
+set_pixel_2()   
+ 
+ 
+a.add_frame(m)
+
+background(R)
+heart(Y)
+a.add_frame(m
+)
+
+ 
+ 
+for set_gjemn in range(8):
+  m[set_gjemn][0] = G
+  m[set_gjemn][1] = G
+  m[set_gjemn][2] = G
+  m[set_gjemn][3] = G
+  m[set_gjemn][4] = G
+  m[set_gjemn][5] = G
+  m[set_gjemn][6] = G
+  m[set_gjemn][7] = G
+
+a.add_frame(m)
+
+
+indate = 50, "Rader kod!"
+def fu(indate):
+  indate = utdate + 50
+
+print()
