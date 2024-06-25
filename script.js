bfb3c2c659ae001a7347012c8b878e0e66b13d3f
FEUE256 <vargard36@gmail.com>
2024-06-25 17:37:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var var1 = document.querySelector("#minElement");
+
+function fu1 (){
+    var var2 = confirm("Vill du skicka de nu?");
+}
+
+if (var2 == false) {
+    var var3 = window.href("127.0.0.1:5500/FE.html");
+}
